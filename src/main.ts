326ca59900c2ec6ae2e7aaabc9cfad220154fba3
ngan <ngannguyen1399@gmail.com>
2023-06-21 12:04:37 +0700

    main entry file

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,17 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+/**
+ * First file that application to run
+ *
+ * 2 Responsibilities:
+ *    - First: need to load angular framework
+ *    - Second: load app code
+ *    => Load angular before load app
+ *
+ * main.ts
+ *    - code inside this loading already loading angular and app code
+ *
+*/
