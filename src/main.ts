0c98b3dda48f0f0e2c46f7b7cfa0f49829492d16
ngan <ngannguyen1399@gmail.com>
2023-06-22 16:36:41 +0700

    enabling production mode

--- src/main.ts
+++ src/main.ts
@@ -18,4 +18,41 @@ platformBrowserDynamic().bootstrapModule(AppModule)
  * main.ts
  *    - code inside this loading already loading angular and app code
  *
-*/
+ */
+
+/**
+ * Enabling Production Mode
+ * Boost the performance of an app by turning on production mode
+ * -------------------------------------------------------------
+ *
+ * Change Detection
+ *    - Synchronizing data in an app with HTML
+ *    => the process to update HTML document whenever data changes in an app.
+ *    For example, if you press a button upload a file, send the request, or typing a form.
+ *    Change Detection will run. Angular will check the data in an app. If the data changes, it will
+ *    reflect those changes in the HTML, instead of refreshing the page like regular site
+ *
+ *    When change detection runs:
+ *      - When the app is initialized
+ *      - During changes in our data
+ *      - Manually triggering change detection
+ *
+ *    => Angular change detection runs twice when an app is initialized.
+ *    => When our app is initialized, it will run change detection twice,
+ *    it doesn't matter if data changes in our app. After change detection twice,
+ *    it will compare the result to both output, if the app output different result,
+ *    that mean we done something wrong. If our data doesn't change, that mean this template
+ *    doesn't change ether. Consistence is the key of any application, user should be able to
+ *    refresh their browser without seen any complete app. This condition check could be help
+ *    for debugging, it rare to make a issue, by it can happen.
+ *    => Don't want it in production app. If leave it enable, it could impact performance of
+ *    our app, we should turn off it when ship our app to production.
+ *    => By default, angular will turn it on, to turn off:
+ *      1. Go to angular.json
+ *      2. projects.basics.architect.build.configurations
+ *      3. Have setting for product and development mode
+ *      4. inside development mode: "optimization": false => if setting to "true", angular will
+ *      not run change detection twice on initialization. Remember, this feature is helpful for
+ *      debugging an app, we could leave on during development. By default, this value is true.
+ *
+ */
